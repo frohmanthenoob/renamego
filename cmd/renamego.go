731156b25c8bb71db42d27a1a6a7fa246d367c6e
frohmanthenoob <cabal_357@hotmaill.com>
2019-02-21 17:38:21 +0800

    feat: basic function

---
+++ cmd/renamego.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+)
+
+func main() {
+
+	currentPath, err := os.Getwd()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	filepath.Walk(currentPath, func(path string, info os.FileInfo, err error) error {
+
+		if err != nil {
+			fmt.Println(err)
+			return err
+		}
+
+		if currentPath == path {
+			return err
+		}
+
+		if info.IsDir() {
+			return filepath.SkipDir
+		}
+
+		pathDir := filepath.Dir(path)
+		fileName := filepath.Base(path)
+		newFileName := fileName
+
+		r := regexp.MustCompile(`[\s,:-]+`)
+		newFileName = r.ReplaceAllString(fileName, "_")
+
+		for strings.Index(newFileName, "__") >= 0 {
+			n := strings.Index(newFileName, "__")
+			newFileName = newFileName[:n] + "_" + newFileName[n+2:]
+		}
+
+		newPath := pathDir + "/" + newFileName
+
+		if path != newPath {
+			fmt.Println(fileName)
+			fmt.Println(newFileName)
+			os.Rename(path, newPath)
+		}
+
+		return err
+	})
+
+}
+
+// GOBIN=$GOPATH/bin go install cmd/namego.go
