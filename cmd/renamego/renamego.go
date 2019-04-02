66b151a5ab561354a3a0a5304ac86838cb0a3f14
frohmanthenoob <cabal_357@hotmail.com>
2019-04-02 20:31:09 +0800

    feat: fileter '.'

--- cmd/renamego/renamego.go
+++ cmd/renamego/renamego.go
@@ -13,6 +13,7 @@ func main() {
 	currentPath, err := os.Getwd()
 	if err != nil {
 		fmt.Println(err)
+		os.Exit(1)
 	}
 
 	filepath.Walk(currentPath, func(path string, info os.FileInfo, err error) error {
@@ -31,17 +32,20 @@ func main() {
 		}
 
 		pathDir := filepath.Dir(path)
+		fileExt := filepath.Ext(path)
 		fileName := filepath.Base(path)
-		newFileName := fileName
+		fileName = strings.TrimSuffix(fileName, fileExt)
 
-		r := regexp.MustCompile(`[\s,，︰:–!-]+`)
-		newFileName = r.ReplaceAllString(fileName, "_")
+		r := regexp.MustCompile(`[\s,，.︰:–!-]+`)
+		newFileName := r.ReplaceAllString(fileName, "_")
 
 		for strings.Index(newFileName, "__") >= 0 {
 			n := strings.Index(newFileName, "__")
 			newFileName = newFileName[:n] + "_" + newFileName[n+2:]
 		}
 
+		fileName = fileName + fileExt
+		newFileName = newFileName + fileExt
 		newPath := pathDir + "/" + newFileName
 
 		if path != newPath {
